9c4029a988c1c6fa084a4a18d52dfb2479210418
ccustodiocode <ccustodio.code@gmail.com>
2020-11-10 17:01:31 -0800

    Initial Commit

---
+++ Jenny/JennyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JennyApp.swift
+//  Jenny
+//
+//  Created by aoi on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct JennyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
